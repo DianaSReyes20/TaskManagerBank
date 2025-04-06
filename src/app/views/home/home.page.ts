2e68a30cc95a895b05f276eb12cab95700f094c9
Sofia Reyes <dianasreyes20@gmail.com>
2025-04-06 02:37:23 -0500

    Listado organizado de tarjetas de tareas  en home

--- src/app/views/home/home.page.ts
+++ src/app/views/home/home.page.ts
@@ -15,7 +15,10 @@ export class HomePage {
     this.taskToShow = [
       { id: 1, title: 'Atender cliente en caja', completed: false, category: 'Caja' },
       { id: 2, title: 'Revisar solicitud de préstamo', completed: true, category: 'Préstamo' },
-      { id: 3, title: 'Ofrecer nuevo producto de ahorro', completed: false, category: 'Nuevo producto' }
+      { id: 3, title: 'Ofrecer nuevo producto de ahorro', completed: false, category: 'Nuevo producto' },
+      { id: 4, title: 'Atender cliente en caja', completed: false, category: 'Caja' },
+      { id: 5, title: 'Revisar solicitud de préstamo', completed: true, category: 'Préstamo' },
+      { id: 6, title: 'Ofrecer nuevo producto de ahorro', completed: false, category: 'Nuevo producto' }
     ];
     this.isLoading = false;
   }
